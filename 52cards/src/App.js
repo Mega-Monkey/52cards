938e35c1c02abfecbb4d0288c5a599221bb3c77f
monkey <sam.corominas@gmail.com>
2019-09-10 12:21:50 +1000

    material UI added app bar and button added

---
+++ 52cards/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ButtonBarApp from './ButtonBarApp'
+import ContainedButtons from './ContainedButtons'
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonBarApp />
+      <ContainedButtons />
+    </div>
+  );
+}
+
+export default App;
--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var question = document.querySelector('.question')
 var input = document.querySelector('input')
 var confirm = document.querySelector('.confirm')
+var aboutNav = document.querySelector('.aboutNav')
+var answersNav = document.querySelector('.answersNav')
+var settingsNav = document.querySelector('.settingsNav')
 var about = document.querySelector('.about')
+var answers = document.querySelector('.answers')
+var popUp = document.querySelector('.popUp')
+var scoreNum = document.querySelector('.scoreNum')
+var tutorial = document.querySelector('.tutorial')
+var answerBox = document.querySelector('.answerBox')
+// modals
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+var modalShow = document.querySelector(".modalShow");
+// modals
 
 numberPlusLetters = { 
     0: 'sz',
@@ -14,49 +28,104 @@ numberPlusWords = {
     1: 'ale',
     2: 'hen',
     3: 'ham',
+    4: 'war',
+    5: 'hive',
+    6: 'bee',
+    7: 'tea',
+    8: 'shoe',
+    9: 'goo',
+    10: 'lice',
+    11: 'lily',
+    12: 'line',
+    13: 'lime',
+    14: 'lorry',
+    15: 'laugh',
+    16: 'lip',
+    17: 'light',
+    18: 'ledge',
+    19: 'leg',
+    20: 'nose',
+    21: 'nail',
+    22: 'nanny',
+    23: 'gnome',
+    24: 'Nero',
+    25: 'knife',
+    26: 'nob',
+    27: 'knife',
+    28: 'notch',
+    29: 'nag',
+    30: 'moss',
+    31: 'mail',
+    32: 'money',
+    33: 'mum',
+    34: 'merry',
+    35: 'muff',
+    36: 'map',
+    37: 'mat'
             }
 
 function newWord() {
-    question.textContent = Math.floor((Math.random() * 3) + 0)
+    if (+scoreNum.textContent === 5) {
+        tutorial.classList.add('show-modal')
+    }
+    question.textContent = Math.floor((Math.random() * 5) + 0)
+}
+function restetQuestionArea() {
+    setTimeout(function(){
+        newWord()
+        confirm.textContent = ''
+        answerBox.value = ''
+        answerBox.disabled = false
+    }, 1000)
 }
 function checkAnswer() {
-    if (input.value === numberPlusWords[question.textContent]) {
-        confirm.textContent = 'correct'
-        setTimeout(function(){
-            newWord()
-            confirm.textContent = ''
-            input.value = ''
-        }, 1000)
+    
+    if (event.key === 'Enter'){
+        if (answerBox.value === numberPlusWords[question.textContent]) {
+            answerBox.disabled = true
+            confirm.textContent = 'correct'
+            scoreNum.textContent = +scoreNum.textContent + 1
+            restetQuestionArea()
+        } else if (answerBox.value !== numberPlusWords[question.textContent]) {
+            answerBox.disabled = true
+            confirm.textContent = 'incorrect'
+            scoreNum.textContent = +scoreNum.textContent - 1
+            restetQuestionArea()
+        }
+    } else {
+        return 
     }
 }
 function aboutPopUp() {
     console.log('hkdjshfk')
 }
 
-$.ajax({
-    url: 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
-}).done(function(res){
-    console.log(res) 
-    newWord()
-})
+newWord()
+// $.ajax({
+//     url: 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+// }).done(function(res){
+// })
 
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
+// pop up 1
+
+
+
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
 
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
+    if (event.target.classList.contains('nav')) {
+        event.target.children[0].classList.add("show-modal");
+        console.log('if yes')
+    } else if (!event.target.classList.contains('closer')) {
+        console.log('if no')
+        document.querySelector('.show-modal').classList.remove("show-modal");
     }
+    // modal.classList.toggle("show-modal");
 }
 
-about.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
 
-input.addEventListener('input', checkAnswer)
-about.addEventListener('click', aboutPopUp)
+aboutNav.addEventListener("click", toggleModal);
+answersNav.addEventListener("click", toggleModal);
+settingsNav.addEventListener("click", toggleModal);
+tutorial.addEventListener('click', toggleModal)
+answerBox.addEventListener('keydown', checkAnswer)
